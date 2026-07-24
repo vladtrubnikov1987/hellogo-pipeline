78d20c5c1793d75b2e799b06a05f7a5f62646e41
vladtrubnikov1987 <vladtrubnikov1987@gmail.com>
2026-07-24 11:51:05 +0300

    Turn hellogo into web server

--- hellogo/main.go
+++ hellogo/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+
+	fmt.Fprintf(w, "shalom, world!\nCurrent time: %s\n", currentTime)
+}
 
 func main() {
-    fmt.Println("hello, world")
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Server started on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
