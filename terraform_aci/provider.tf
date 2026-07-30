e94756cffef193e83847a2bf9deb48654716b715
vladtrubnikov1987 <vladtrubnikov1987@gmail.com>
2026-07-30 19:54:06 +0300

    Configure Azure remote state backend

--- terraform_aci/provider.tf
+++ terraform_aci/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-vladimir-tfstate"
+    storage_account_name = "stvladimirtfstate1987"
+    container_name       = "tfstate"
+    key                  = "hellogo-aci.tfstate"
+  }
 }
 
 provider "azurerm" {
